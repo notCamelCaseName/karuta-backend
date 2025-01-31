674ef07d2592ec64ec44676cc93046a9ac3aac57
Louis Delyon <louis.delyon+git@proton.me>
2025-01-31 14:03:41 +0100

    Added basic swagger

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,22 @@ mod cors;
 use cors::*;
 
 use rocket::{fs::NamedFile, State};
+use rocket_okapi::{openapi, openapi_get_routes, swagger_ui::{make_swagger_ui, SwaggerUIConfig}};
 use std::{path::Path, sync::Arc};
 
+#[openapi(tag = "Decks")]
 #[get("/deck/metadata/<name>")]
 async fn deck_metadata(decks: &State<Arc<Vec<Deck>>>, name: &str) -> Option<String> {
     serde_json::to_string(decks.iter().find(|deck| deck.name == name)?).ok()
 }
 
+#[openapi(tag = "Decks")]
 #[get("/deck/names")]
 fn deck_names(decks: &State<Arc<Vec<Deck>>>) -> String {
     decks.iter().map(|deck| deck.name.clone() + "\n").collect()
 }
 
+#[openapi(tag = "Decks")]
 #[get("/visual/<name>")]
 async fn get_visual(name: &str) -> Option<NamedFile> {
     NamedFile::open(Path::new(&format!("decks/Visuals/{name}")))
@@ -27,6 +31,7 @@ async fn get_visual(name: &str) -> Option<NamedFile> {
         .ok()
 }
 
+#[openapi(tag = "Decks")]
 #[get("/sound/<name>")]
 async fn get_sound(name: &str) -> Option<NamedFile> {
     NamedFile::open(Path::new(&format!("decks/Sounds/{name}")))
@@ -34,6 +39,7 @@ async fn get_sound(name: &str) -> Option<NamedFile> {
         .ok()
 }
 
+#[openapi(tag = "Decks")]
 #[get("/deck/cover/<name>")]
 async fn get_cover(name: &str) -> Option<NamedFile> {
     NamedFile::open(Path::new(&format!("decks/Covers/{name}")))
@@ -41,6 +47,7 @@ async fn get_cover(name: &str) -> Option<NamedFile> {
         .ok()
 }
 
+#[openapi(tag = "Themes")]
 #[get("/theme/names")]
 fn theme_names() -> String {
     std::fs::read_dir("decks/Themes")
@@ -51,6 +58,7 @@ fn theme_names() -> String {
         .collect()
 }
 
+#[openapi(tag = "Themes")]
 #[get("/theme/<name>")]
 async fn get_theme(name: &str) -> Option<NamedFile> {
     NamedFile::open(Path::new(&format!("decks/Themes/{name}")))
@@ -58,21 +66,25 @@ async fn get_theme(name: &str) -> Option<NamedFile> {
         .ok()
 }
 
+#[openapi(tag = "Categories")]
 #[get("/categories")]
 async fn get_categories(categories: &State<Arc<CategoryJSON>>) -> Option<String> {
     serde_json::to_string(&categories.categories).ok()
 }
 
+#[openapi(tag = "Categories")]
 #[get("/types")]
 async fn get_types(categories: &State<Arc<CategoryJSON>>) -> Option<String> {
     serde_json::to_string(&categories.types).ok()
 }
 
+#[openapi(tag = "Categories")]
 #[get("/categories_and_types")]
 async fn get_categories_and_types(categories: &State<Arc<CategoryJSON>>) -> Option<String> {
     serde_json::to_string(categories.inner().as_ref()).ok()
 }
 
+#[openapi(tag = "Categories")]
 #[get("/category/icon/<name>")]
 async fn get_category_icon(name: &str, categories: &State<Arc<CategoryJSON>>) -> Option<NamedFile> {
     let icon_path = &categories
@@ -102,7 +114,7 @@ fn rocket() -> _ {
         .attach(CORS)
         .mount(
             "/",
-            routes![
+            openapi_get_routes![
                 deck_metadata,
                 deck_names,
                 theme_names,
@@ -115,6 +127,13 @@ fn rocket() -> _ {
                 get_categories_and_types,
                 get_category_icon,
             ],
+        )
+        .mount(
+            "/swagger-ui/",
+            make_swagger_ui(&SwaggerUIConfig {
+                url: "../openapi.json".to_owned(),
+                ..Default::default()
+            }),
         )
         .manage(Arc::new(decks))
         .manage(Arc::new(categories))
